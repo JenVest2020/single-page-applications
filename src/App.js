34f1c86df156cb9e355d11ff558aa5a9660dfc71
JenVest2020 <jenvest2019@gmail.com>
2020-06-12 12:25:07 -0500

    home page almost complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { BrowserRouter as router, Route, Link } from 'react-router-dom';
+import { Navbar, Button, Card, CardImg } from "reactstrap";
 
 const App = () => {
   return (
     <>
-      <h1>WeEat</h1>
+      <Navbar color='info'>
+        <h1>WeEat</h1>
+        <Button color='info' style={{ color: 'white' }}>
+          Home
+      </Button>
+      </Navbar>
+      <Route exact path='/'><Card>
+        <Button color='info' style={{ color: 'white', position: 'absolute', left: '50%', top: '50%' }}>
+          Tacos!
+        </Button>
+        <CardImg src={require('./assets/taco.jpeg')} />
+      </Card>
+      </Route>
+      <Route path='/taco'>
+
+      </Route>
+
     </>
-  );
+
+  )
 };
 export default App;
