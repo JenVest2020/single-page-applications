5cdf7d7d2a141593393b05b40e28d948240a29c0
JenVest2020 <jenvest2019@gmail.com>
2020-06-12 15:51:40 -0500

    completed form and installed axios&yup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { BrowserRouter as router, Route, Link } from 'react-router-dom';
 import { Navbar, Button, Card, CardImg } from "reactstrap";
+import OrderForm from './OrderForm';
 
 const App = () => {
   return (
     <>
       <Navbar color='info'>
         <h1>WeEat</h1>
-        <Button color='info' style={{ color: 'white' }}>
-          Home
+        <Link to='/'>
+          <Button color='info' style={{ color: 'white' }}>
+            Home
       </Button>
+        </Link>
       </Navbar>
-      <Route exact path='/'><Card>
-        <Button color='info' style={{ color: 'white', position: 'absolute', left: '50%', top: '50%' }}>
-          Tacos!
+      <Route exact path='/'>
+        <Card>
+          <Link to='/taco'>
+            <Button color='info' style={{ color: 'white', position: 'absolute', left: '50%', top: '50%' }}>
+              Tacos!
         </Button>
-        <CardImg src={require('./assets/taco.jpeg')} />
-      </Card>
+          </Link>
+          <CardImg src={require('./assets/taco.jpeg')} />
+        </Card>
       </Route>
       <Route path='/taco'>
-
+        <OrderForm />
       </Route>
 
     </>
---
+++ src/OrderForm.js
@@ -0,0 +1,171 @@
+import React, { useState } from "react";
+import { BrowserRouter as router, Route, Link } from 'react-router-dom';
+import { Form, FormGroup, Dropdown, Input, Card, CardImg, DropdownToggle, DropdownMenu, label, Button } from "reactstrap";
+
+
+
+
+const OrderForm = () => {
+    const [DropDownOpen, setDropDownOpen] = useState(false);
+    const toggle = () => setDropDownOpen((prevState) => !prevState);
+    const [formData, setFormData] = useState({
+        name: '',
+        number: 0,
+        sauce: '',
+        meat: '',
+        cilantro: false,
+        whiteOnion: false,
+        lime: false,
+        avacado: false,
+        special: ''
+    });
+
+    const onInputChange = (e) => {
+        // e.persist()
+        // validateChange(e)
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleToppings = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.checked
+        });
+    }
+
+    return (
+        <>
+            <Card color='info'>
+                <h2 style={{ color: 'white', margin: '0 auto' }}>
+                    Build Your Own Taco!
+                </h2>
+                <CardImg style={{ width: '80%', margin: '0 auto' }} src={require('./assets/taco-2.jpg')} />
+            </Card>
+            <Form onSubmit={(e) => {
+                e.preventDefault();
+                console.log('from Form', formData);
+            }} style={{ margin: '5%' }}>
+                <FormGroup>
+                    <legend>Name</legend>
+                    <Input type='name' name='name' value={formData.name} onChange={onInputChange} />
+                </FormGroup>
+                <FormGroup>
+                    <Dropdown isOpen={DropDownOpen} toggle={toggle}>
+                        <DropdownToggle caret>
+                            {formData.number === 0 ? 'Number of Tacos' : formData.number}
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <div onClick={() => {
+                                toggle();
+                                setFormData({ number: 0 })
+                            }}>0</div>
+                            <div onClick={() => {
+                                toggle();
+                                setFormData({ number: 1 })
+                            }}>1</div>
+                            <div onClick={() => {
+                                toggle();
+                                setFormData({ number: 2 })
+                            }}>2</div>
+                            <div onClick={() => {
+                                toggle();
+                                setFormData({ number: 3 })
+                            }}>3</div>
+                            <div onClick={() => {
+                                toggle();
+                                setFormData({ number: 4 })
+                            }}>4</div>
+                        </DropdownMenu>
+                    </Dropdown>
+                </FormGroup>
+                <FormGroup tag='fieldset'>
+                    <legend>Sauce</legend>
+                    <FormGroup check>
+                        <label check>
+                            <input type='radio' name='sauce' value='Red' onChange={onInputChange} />
+                            Red
+                        </label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <label check>
+                            <input type='radio' name='sauce' value='Green' onChange={onInputChange} />
+                            Green
+                        </label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <label check>
+                            <input type='radio' name='sauce' value='Chipotle Mayo' onChange={onInputChange} />
+                            Chipotle Mayo
+                        </label>
+                    </FormGroup>
+                    <FormGroup check>
+                        <label check>
+                            <input type='radio' name='sauce' value='None' onChange={onInputChange} />
+                            None
+                        </label>
+                    </FormGroup>
+                </FormGroup>
+                <legend>Meats</legend>
+                <FormGroup check>
+                    <label check>
+                        <input type='radio' name='meat' value='beef' onChange={onInputChange} />
+                            Beef
+                        </label>
+                </FormGroup>
+                <FormGroup check>
+                    <label check>
+                        <input type='radio' name='meat' value='chicken' onChange={onInputChange} />
+                            Chicken
+                        </label>
+                </FormGroup>
+                <FormGroup check>
+                    <label check>
+                        <input type='radio' name='meat' value='fish' onChange={onInputChange} />
+                            Fish
+                        </label>
+                </FormGroup>
+                <FormGroup check>
+                    <label check>
+                        <input type='radio' name='meat' value='pork' onChange={onInputChange} />
+                            Pork
+                        </label>
+                </FormGroup>
+                <legend>Toppings</legend>
+                <FormGroup check>
+                    <label check>
+                        <input type='checkbox' name='cilantro' checked={formData.cilantro} onChange={handleToppings} />
+                        Cilantro
+                    </label>
+                </FormGroup>
+                <FormGroup check>
+                    <label check>
+                        <input type='checkbox' name='whiteOnion' checked={formData.whiteOnion} onChange={handleToppings} />
+                        White Onion
+                    </label>
+                </FormGroup>
+                <FormGroup check>
+                    <label check>
+                        <input type='checkbox' name='lime' checked={formData.lime} onChange={handleToppings} />
+                        Lime
+                    </label>
+                </FormGroup>
+                <FormGroup check>
+                    <label check>
+                        <input type='checkbox' name='avacado' checked={formData.avacado} onChange={handleToppings} />
+                        Avacado
+                    </label>
+                </FormGroup>
+                <FormGroup>
+                    <legend>Special Instructions</legend>
+                    <input type='textarea' name='special' value={formData.special} onChange={onInputChange} />
+                </FormGroup>
+                <Button>Add to Order</Button>
+            </Form>
+        </>
+    )
+};
+
+export default OrderForm;
